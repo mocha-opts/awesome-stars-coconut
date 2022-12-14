9140d0527eb14f75f8a7e15fbde332c797a805bc
coconut <1424392205@qq.com>
2022-12-14 22:42:38 +0800

    chore: update config and workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: isProd ? "/awesome-stars-coconut/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig
